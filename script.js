9f492103009cfe157fe9be1395c9310a82d850a9
trezmor <104760231+trezmor@users.noreply.github.com>
2023-01-16 01:14:38 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict"
+function activateState(){
+	document.getElementById("containerFooter").style.visibility = "hidden"
+	document.getElementById("shareOptions").style.visibility = "visible"
+}
+
+
+
+
+
+/* 
+inactive: 
+containeFooter needs:
+visibility: visible;
+z-index: 1;
+
+shareOptions:
+containeFooter needs:
+visibility: invisible;
+z-index: 0; 
+
+
+active:
+containeFooter needs:
+visibility: invisible;
+z-index: 0;
+
+shareOptions:
+containeFooter needs:
+visibility: visible;
+z-index: 1; 
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
